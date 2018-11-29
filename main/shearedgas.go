7ae145966c9e4a3e8d80456a1d03c93bc13c8581
yernurb <yernurb@gmail.com>
2018-11-29 17:45:35 +0600

    Tested Vec2 structure

--- main/shearedgas.go
+++ main/shearedgas.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/yernurb/shearedgas/vector"
+)
 
 func main() {
+	v1 := vector.Vec2{1.2, 2.3}
+	v2 := vector.Vec2{-4.3, 2.1}
 	fmt.Println("Hello, World")
+	fmt.Println(v1.X, v1.Y)
+	fmt.Println(v2.X, v2.Y)
+	v1.X = 4.0
+	v1.Y = 3.0
+	fmt.Println(v1.Norm2())
 }
